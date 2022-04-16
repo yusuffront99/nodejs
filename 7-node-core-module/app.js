b45c2a7fb7bd7a9d986e746d2a301825a28750c9
yusuf <yusuffront@gmail.com>
2022-04-16 09:32:49 +0800

    new folder --lesson part module

--- 7-node-core-module/app.js
+++ 7-node-core-module/app.js
@@ -2,7 +2,7 @@
 // filesystem
 
 const fs = require('fs');
-console.log(fs);
+// console.log(fs);
 
 // menuliskan file secara syncronous
 // try {
@@ -27,12 +27,48 @@ console.log(fs);
 // console.log(data1);
 
 // Membaca ISI FILE ASYNCRONOUS
-fs.readFile('data/text.txt', (e, data) => {
-    if(e) throw e;
-    console.log(data.toString());
-})
+// fs.readFile('data/text.txt', (e, data) => {
+//     if(e) throw e;
+//     console.log(data.toString());
+// })
 
-fs.readFile('data/text.txt', 'utf-8', (e, data) => {
-    if(e) throw e;
-    console.log(data);
-})
+// fs.readFile('data/text.txt', 'utf-8', (e, data) => {
+//     if(e) throw e;
+//     console.log(data);
+// })
+
+
+//========READLINE
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout,
+// });
+
+// rl.question('What is your name : ', (name) => {
+//     rl.question('Enter you\'re age : ', (age) => {
+//         console.log(`My name is : ${name}, My age is : ${age}`);
+//         rl.close;
+//     });
+// });
+
+// ============ SAVE DATA TO JSON
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question('What is your name : ', (name) => {
+    rl.question('Enter you\'re age : ', (age) => {
+            const contact = {name, age};
+            const file = fs.readFileSync('data/contacts.json', 'utf-8');
+            const contacts = JSON.parse(file);
+
+            contacts.push(contact);
+
+            // console.log(data);
+            fs.writeFileSync('data/contacts.json', JSON.stringify(contacts));
+            rl.close;
+    });
+});
